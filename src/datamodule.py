dea54c7b4905b4cbe3c1a868b49f9fd9a4b28fd4
waybaba <wwang828@uwo.ca>
2023-09-03 16:02:36 +0000

    get kuka here

--- src/datamodule.py
+++ src/datamodule.py
@@ -14,6 +14,7 @@ import os
 from glob import glob
 import gymnasium as gym
 import pybullet_envs
+from gym_stacking.env import StackEnv
 
 
 Batch = namedtuple('Batch', 'trajectories conditions')
@@ -65,7 +66,7 @@ def load_kuka(env, custom_ds_path):
 	# qstates = qstates[:i+1]
 	# path_lengths = path_lengths[:i+1]
 	# return qstates, path_lengths
-	return qstates, gym.make(env)
+	return qstates, StackEnv()
 
 
 ### dataset
